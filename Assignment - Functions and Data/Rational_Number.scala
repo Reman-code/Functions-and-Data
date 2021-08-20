2d9f4f5d722f7eb99049fc1c93f6b5492e8d3269
Thilina Madusanka <77828519+Reman-code@users.noreply.github.com>
2021-08-20 20:02:04 +0530

    Functions and Data

---
+++ Assignment - Functions and Data/Rational_Number.scala
@@ -0,0 +1,9 @@
+
+object RationalNumbers extends App{
+    val w = new Rational(3)
+    val x = new Rational(3,4)
+    val y = new Rational(5,8)
+    val z = new Rational(2,7)
+    val t = x - y - z
+    println(t)
+}
